85a5f66c119b53a9a5200d0b31a735e81d8d8025
Pratik Bhadane <pratikbhadane24@gmail.com>
2021-09-16 18:05:53 +0530

    bug fix

--- main.py
+++ main.py
@@ -20,10 +20,11 @@ def RealtimeCoverage():
         faces = face_cascade.detectMultiScale(gray, 1.3, 5)
         for (x, y, w, h) in faces:
             cv2.rectangle(img, (x, y), (x+w, y+h), (182, 25, 25), 2)
-            roi_gray = gray[y:y+h, x:x+w]
-            roi_color = img[y:y+h, x:x+w]
-
+        
         # Eyes Detection
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (255, 235, 161), 2)
